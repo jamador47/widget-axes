415df0da2fbcb9e807728003988209800ebbfd7c
jamador47 <jamador@unitec.edu>
2018-10-30 22:34:48 -0600

    Revert "cambios DIDA"
    This reverts commit 98c87f10b554ad271a2fc03774096ff5e5fe9a11.

--- widget.js
+++ widget.js
@@ -1421,11 +1421,7 @@ cpdefine("inline:com-chilipeppr-widget-xyz", ["chilipeppr_ready", "jquerycookie"
             // Homes all axes present in command. At least one axis letter must be present. The value (number) must be provided but is ignored.
             // The homing sequence is fixed and always starts with the Z axis (if requested). The sequence runs ZXYA (but skipping all axes that are not specified in the G28.2 command)
             console.log("homeAxis. evt.data:", evt.data, "evt:", evt);
-            
-this.publishSend("G0 z1000 \n");
-            this.publishSend("$clear \n");
-        
-            
+            this.homePLC();
             var cmd = "G28.2 ";
             if (evt.data == "xyz") {
                 cmd += "X0 Y0 Z0";
@@ -1438,8 +1434,7 @@ this.publishSend("G0 z1000 \n");
             cmd += "\n";
             console.log(cmd);
             //chilipeppr.publish("/com-chilipeppr-widget-serialport/send", cmd);
-      
-            this.homePLC();
+            this.publishSend(cmd);
 
         },
         homePLC: function(evt){
